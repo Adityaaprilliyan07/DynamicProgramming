c55ba15ecc0e809545c8b94c9222a34041d455ad
Asus <Asus@DESKTOP-R7PQKTL>
2018-09-12 13:02:55 +0700

    Tambah Hitung Fibonacci

--- src/hitungfibonacci/HitungFibonacci.java
+++ src/hitungfibonacci/HitungFibonacci.java
@@ -5,6 +5,9 @@
  */
 package hitungfibonacci;
 
+import java.math.BigInteger;
+import java.util.Scanner;
+
 /**
  *
  * @author Asus
@@ -22,6 +25,26 @@ public class HitungFibonacci {
     public static void main(String[] args ){
         String identitas = "Aditya Aprilliyan Putra / XR5 / 02";
         tampilJudul(identitas);
+        int n = tampilInput();
+        BigInteger hasil = fibo(n);
     }
+        private static int tampilInput(){
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Bilangan ke-1: ");
+            int n = scanner.nextInt();
+            
+            return n;
+    }
+       private static BigInteger fibo(int n){
+           BigInteger[] hasil = new BigInteger[n];
+           hasil[0] = BigInteger.ONE;
+           hasil[1] = BigInteger.ONE;
+           for (int i = 2; i < n; i++) {
+               hasil[i] = hasil[i-1].add(hasil[i-2]);
+           }
+           
+           return hasil[n-1];
+           
+       }
     
 }
